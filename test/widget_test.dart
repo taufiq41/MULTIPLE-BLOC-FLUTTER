66d4b2af6a5c0fa1a9854afb409a65eb9c8fa9ad
Triastono Taufiq <triastonotaufiq35@gmail.com>
2020-05-13 10:02:01 +0700

    commit finishing single-bloc

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:paratice/ui/single_bloc_app.dart';
 import 'package:paratice/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(SingleBlocApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
